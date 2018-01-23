9aa310c12de6b98c1ed3a2bdb7e2de3ef4c8ea45
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2018-01-23 12:35:44 +0100

    unworking attempt at extractors

--- src/main/scala/ch/dubernet/matsimconsole/matsiminterface/Coord.scala
+++ src/main/scala/ch/dubernet/matsimconsole/matsiminterface/Coord.scala
@@ -1,12 +1,22 @@
 package ch.dubernet.matsimconsole.matsiminterface
 
-case class Coord( val x: Double, val y: Double )
-case class CoordZ( override val x: Double, override val y: Double, z: Double ) extends Coord(x,y)
+class Coord( val x: Double, val y: Double , val z: Option[Double] )
+class Coord( val x: Double, val y: Double , val z: Option[Double] )
 
 object Coord {
   private type MatsimCoord = org.matsim.api.core.v01.Coord
+
+  def apply( x: Double, y: Double ) = new Coord( x , y , None )
+  def apply( x: Double, y: Double , z: Double ) = new Coord( x , y , Some( z ) )
+
   def apply( matsimCoord: MatsimCoord ): Coord = {
-    if ( matsimCoord.hasZ ) CoordZ( matsimCoord.getX , matsimCoord.getY , matsimCoord.getZ )
+    if ( matsimCoord.hasZ ) Coord( matsimCoord.getX , matsimCoord.getY , matsimCoord.getZ )
     else Coord( matsimCoord.getX , matsimCoord.getY )
+  }
+
+  def unapply( coord: Coord ): Option[(Double,Double)] = Some((coord.x , coord.y))
+  def unapply( coord: Coord ): Option[(Double,Double,Double)] = coord.z match {
+    case Some( z ) => Some( (coord.x , coord.y , z ) )
+    case None => None
   }
 }
