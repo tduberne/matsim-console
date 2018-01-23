d347654d2ee3d98fb5640b1e8ab694255f9b1532
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2018-01-23 13:02:56 +0100

    better test names

--- src/test/scala/ch/dubernet/matsimconsole/matsiminterface/CoordTest.scala
+++ src/test/scala/ch/dubernet/matsimconsole/matsiminterface/CoordTest.scala
@@ -3,21 +3,21 @@ package ch.dubernet.matsimconsole.matsiminterface
 import org.scalatest.FunSuite
 
 class CoordTest extends FunSuite {
-  test( "pattern match coord" ) {
+  test( "pattern match 2D coord" ) {
     Coord( 1 , 2 ) match {
       case Coord2D( 1 , 2 ) =>
       case _ => fail()
     }
   }
 
-  test( "pattern match coordZ" ) {
+  test( "pattern match 3D coord" ) {
     Coord( 1 , 2 , 3 ) match {
       case Coord3D( 1 , 2 , 3 ) =>
       case _ => fail()
     }
   }
 
-  test( "coord pattern matches coordZ" ) {
+  test( "2D coord pattern matches 3D coord" ) {
     Coord( 1 , 2 , 3 ) match {
       case Coord2D( 1 , 2 ) =>
       case _ => fail()
